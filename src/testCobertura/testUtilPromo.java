9abf45bbfc25c9c56dd1c6d6248243f6b6dfc4b7
FedeGarcia00 <fedegarciamdq@gmail.com>
2023-11-10 10:57:30 -0300

    cobertura andando

--- src/testCobertura/testUtilPromo.java
+++ src/testCobertura/testUtilPromo.java
@@ -1,16 +1,23 @@
 package testCobertura;
 
 
-import static org.junit.Assert.*;
+
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import modeloDatos.Cliente;
+import modeloDatos.ClientePuntaje;
 import modeloDatos.EmpleadoPretenso;
 import modeloDatos.Empleador;
+import util.Constantes;
 
 public class testUtilPromo {
 
@@ -25,21 +32,102 @@ public class testUtilPromo {
 	}
 
 	@Test
-	public void test1() {
+	public void testC1() {
 		HashMap<String, EmpleadoPretenso> empleados = new HashMap<String, EmpleadoPretenso>();
 		HashMap<String, Empleador> empleadores = new HashMap<String, Empleador>() ;
 		aplicaPromo(false, empleados, empleadores);
+		Cliente resultado = prueba.aplicaPromo(false, empleados, empleadores);
+		Assert.assertEquals("Deberia retornar null",null, resultado);
 	}
 	
 	@Test
-	public void test2() {
+	public void testC2() {
 		HashMap<String, EmpleadoPretenso> empleados = new HashMap<String, EmpleadoPretenso>();
 		HashMap<String, Empleador> empleadores = new HashMap<String, Empleador>() ;
 		aplicaPromo(true, empleados, empleadores);
-		prueba.aplicaPromo(true, empleados, empleadores);
+		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
+		Assert.assertEquals("Deberia retornar null",null, resultado);
 	}
-
-
+////////	
+////	
+	@Test
+	public void testC3() {
+		HashMap<String, EmpleadoPretenso> empleados = new HashMap<String, EmpleadoPretenso>();
+		HashMap<String, Empleador> empleadores = new HashMap<String, Empleador>() ;
+		Empleador empleador1 = new Empleador("fede", "123", "Federico", "22345", Constantes.SALUD, Constantes.FISICA);
+		empleador1.setPuntaje(20);
+		Empleador empleador2 = new Empleador("baucho", "456", "Bautista", "22378", Constantes.SALUD, Constantes.FISICA);
+		empleador2.setPuntaje(10);
+		empleadores.put("1", empleador1);
+		empleadores.put("2", empleador2);
+		aplicaPromo(false, empleados, empleadores);
+		Cliente resultado = prueba.aplicaPromo(false, empleados, empleadores);
+		Assert.assertEquals("Deberia retornar el primer elemento del hashmap",resultado.getUsserName(), "fede");
+	}
+//	
+//	
+//	
+	@Test
+	public void testC4() {
+		HashMap<String, EmpleadoPretenso> empleados = new HashMap<String, EmpleadoPretenso>();
+		HashMap<String, Empleador> empleadores = new HashMap<String, Empleador>() ;	
+		Empleador empleador1 = new Empleador("fede", "123", "Federico", "22345", Constantes.SALUD, Constantes.FISICA);
+		empleador1.setPuntaje(20);
+		empleador1.setListaDePostulantes(null);
+		empleadores.put("1", empleador1);
+		aplicaPromo(true, empleados, empleadores);
+		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
+		Assert.assertEquals("Deberia retornar null",null, resultado);
+	}
+//	
+	@Test
+	public void testC5() {
+		HashMap<String, EmpleadoPretenso> empleados = new HashMap<String, EmpleadoPretenso>();
+		HashMap<String, Empleador> empleadores = new HashMap<String, Empleador>() ;	
+		EmpleadoPretenso empleado1 = new EmpleadoPretenso("santi", "123", "Santiago", "22456", "qwewrty", 23);
+		empleado1.setPuntaje(20);
+		empleado1.setListaDePostulantes(null);
+		empleados.put("1", empleado1);
+		aplicaPromo(true, empleados, empleadores);
+		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
+		Assert.assertNotEquals("Deberia retornar el cliente del hashmap",null, resultado);
+	}
+	
+	@Test
+	public void testC6() {
+		HashMap<String, EmpleadoPretenso> empleados = new HashMap<String, EmpleadoPretenso>();
+		HashMap<String, Empleador> empleadores = new HashMap<String, Empleador>() ;	
+		
+		Empleador empleador1 = new Empleador("fede", "123", "Federico", "22345", Constantes.SALUD, Constantes.FISICA);
+		empleador1.setPuntaje(20);
+		ClientePuntaje clientepuntaje = new ClientePuntaje();
+		ArrayList<ClientePuntaje> listaPostulantes = new ArrayList<ClientePuntaje>();
+		listaPostulantes.add(clientepuntaje);
+		empleador1.setListaDePostulantes(listaPostulantes);
+		empleadores.put("1", empleador1);
+		aplicaPromo(true, empleados, empleadores);
+		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
+		System.out.println(resultado);
+		Assert.assertNotEquals("Deberia retornar el cliente del hashmap",null, resultado);
+	}
+	
+	@Test
+	public void testC7() {
+		HashMap<String, EmpleadoPretenso> empleados = new HashMap<String, EmpleadoPretenso>();
+		HashMap<String, Empleador> empleadores = new HashMap<String, Empleador>() ;		
+		EmpleadoPretenso empleado1 = new EmpleadoPretenso("santi", "123", "Santiago", "22456", "qwewrty", 23);
+		empleado1.setPuntaje(20);
+		ClientePuntaje clientepuntaje = new ClientePuntaje();
+		ArrayList<ClientePuntaje> listaPostulantes = new ArrayList<ClientePuntaje>();
+		listaPostulantes.add(clientepuntaje);
+		empleado1.setListaDePostulantes(listaPostulantes);
+		empleados.put("1", empleado1);
+		aplicaPromo(true, empleados, empleadores);
+		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
+		System.out.println(resultado);
+		Assert.assertNotEquals("Deberia retornar el cliente del hashmap",null, resultado);
+	}
+	
 
 
 	public Cliente aplicaPromo(boolean promoPorListaDePostulantes, 
