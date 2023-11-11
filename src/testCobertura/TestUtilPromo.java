af13da92a2c82d9fe68edf34a4efa55522460c3f
FedeGarcia00 <fedegarciamdq@gmail.com>
2023-11-11 18:18:13 -0300

    correcciones

--- src/testCobertura/TestUtilPromo.java
+++ src/testCobertura/TestUtilPromo.java
@@ -52,8 +52,8 @@ public class TestUtilPromo {
 		empleador1.setPuntaje(20);
 		Empleador empleador2 = new Empleador("baucho", "456", "Bautista", "22378", Constantes.SALUD, Constantes.FISICA);
 		empleador2.setPuntaje(10);
-		empleadores.put("1", empleador1);
-		empleadores.put("2", empleador2);
+		empleadores.put("fede", empleador1);
+		empleadores.put("baucho", empleador2);
 		Cliente resultado = prueba.aplicaPromo(false, empleados, empleadores);
 		Assert.assertEquals("Deberia retornar el primer elemento del hashmap",resultado.getUsserName(), "fede");
 	}
@@ -63,7 +63,7 @@ public class TestUtilPromo {
 		Empleador empleador1 = new Empleador("fede", "123", "Federico", "22345", Constantes.SALUD, Constantes.FISICA);
 		empleador1.setPuntaje(20);
 		empleador1.setListaDePostulantes(null);
-		empleadores.put("1", empleador1);
+		empleadores.put("fede", empleador1);
 		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
 		Assert.assertEquals("Deberia retornar null",null, resultado);
 	}
@@ -73,7 +73,7 @@ public class TestUtilPromo {
 		EmpleadoPretenso empleado1 = new EmpleadoPretenso("santi", "123", "Santiago", "22456", "qwewrty", 23);
 		empleado1.setPuntaje(20);
 		empleado1.setListaDePostulantes(null);
-		empleados.put("1", empleado1);
+		empleados.put("santi", empleado1);
 		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
 		Assert.assertNotEquals("Deberia retornar el cliente del hashmap",null, resultado);
 	}
@@ -86,7 +86,7 @@ public class TestUtilPromo {
 		ArrayList<ClientePuntaje> listaPostulantes = new ArrayList<ClientePuntaje>();
 		listaPostulantes.add(clientepuntaje);
 		empleador1.setListaDePostulantes(listaPostulantes);
-		empleadores.put("1", empleador1);
+		empleadores.put("fede", empleador1);
 		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
 		System.out.println(resultado);
 		Assert.assertNotEquals("Deberia retornar el cliente del hashmap",null, resultado);
@@ -100,7 +100,7 @@ public class TestUtilPromo {
 		ArrayList<ClientePuntaje> listaPostulantes = new ArrayList<ClientePuntaje>();
 		listaPostulantes.add(clientepuntaje);
 		empleado1.setListaDePostulantes(listaPostulantes);
-		empleados.put("1", empleado1);
+		empleados.put("santi", empleado1);
 		Cliente resultado = prueba.aplicaPromo(true, empleados, empleadores);
 		System.out.println(resultado);
 		Assert.assertNotEquals("Deberia retornar el cliente del hashmap",null, resultado);
--- src/testIntegracion/TestIntegracion.java
+++ src/testIntegracion/TestIntegracion.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import modeloDatos.Cliente;
 import modeloDatos.ClientePuntaje;
+import modeloDatos.Contratacion;
 import modeloDatos.EmpleadoPretenso;
 import modeloDatos.Empleador;
 import modeloDatos.Usuario;
@@ -33,69 +34,24 @@ public class TestIntegracion {
 
   @Before
   public void setUp() throws Exception {
-    //Preparación del escenario, ya existen empleados en el sistema
-	 
+    //Preparación del escenario, ya existen empleados en el sistema 
     agencia = Agencia.getInstance();
     agencia.setEstadoContratacion(false);
     HashMap<String, EmpleadoPretenso> empleados = new HashMap<>();
     HashMap<String, Empleador> empleadores = new HashMap<>();
     agencia.setEmpleados(empleados);
     agencia.setEmpleadores(empleadores);
-    empleador1 = agencia.registroEmpleador(
-      "santi",
-      "456",
-      "Santiago",
-      "43234",
-      Constantes.JURIDICA,
-      Constantes.SALUD
-    );
-    agencia.registroEmpleador(
-      "pepe",
-      "765",
-      "Pedro",
-      "21334",
-      Constantes.FISICA,
-      Constantes.SALUD
-    );
-    empleado1 =
-      agencia.registroEmpleado(
-        "buacho",
-        "123",
-        "Bautista",
-        "Orte",
-        "223543",
-        23
-      );
-    empleado2 =
-      agencia.registroEmpleado(
-        "fede",
-        "345",
-        "Federico",
-        "Garcia",
-        "22321",
-        23
-      );
+    empleador1 = agencia.registroEmpleador("santi","456","Santiago","43234",Constantes.JURIDICA,Constantes.SALUD);
+    agencia.registroEmpleador("pepe","765","Pedro","21334",Constantes.FISICA,Constantes.SALUD);
+    empleado1 = agencia.registroEmpleado("buacho","123","Bautista","Orte","223543",23);
+    empleado2 = agencia.registroEmpleado("fede","345","Federico","Garcia","22321",23);
 
     // Se le crea un ticket al empleador 1
-    agencia.crearTicketEmpleador(
-      Constantes.HOME_OFFICE,
-      50000,
-      Constantes.JORNADA_MEDIA,
-      Constantes.JUNIOR,
-      Constantes.EXP_MEDIA,
-      Constantes.TERCIARIOS,
-      empleador1
-    );
+    agencia.crearTicketEmpleador(Constantes.HOME_OFFICE,50000,Constantes.JORNADA_MEDIA,Constantes.JUNIOR,Constantes.EXP_MEDIA,Constantes.TERCIARIOS,empleador1);
     // Se le crea un ticket al empleado 2
-    agencia.crearTicketEmpleado(
-      Constantes.HOME_OFFICE,
-      50000,
-      Constantes.JORNADA_MEDIA,
-      Constantes.JUNIOR,
-      Constantes.EXP_MEDIA,
-      Constantes.TERCIARIOS,
-      empleado2
-    );
+    agencia.crearTicketEmpleado(Constantes.HOME_OFFICE,50000,Constantes.JORNADA_MEDIA,Constantes.JUNIOR,Constantes.EXP_MEDIA,Constantes.TERCIARIOS,empleado2);
+    
+    agencia.setContrataciones(new ArrayList<Contratacion>());
   }
 
   @After
@@ -120,55 +76,54 @@ public class TestIntegracion {
     agencia.cerrarSesion();
 
     // verificamos solo los estados finales, pues se supone que los metodos ya fueron testeados en test unitarios
-    Assert.assertEquals(agencia.getTipoUsuario(), -1);
+    Assert.assertEquals("El estado del usuario al cerrar sesion debe ser -1", -1,agencia.getTipoUsuario());
     Assert.assertEquals(agencia.getLimiteInferior(), 1000);
     Assert.assertEquals(agencia.getLimiteSuperior(), 1500);
     Assert.assertEquals(agencia.getEstado(), Mensajes.AGENCIA_EN_CONTRATACION.getValor());
   }
 
+  
   // 2) Un empleado se registra,se loguea,el estado de contratacion esta en false, por lo tanto crea un ticket
   // y luego cierra sesion
   @Test
   public void testCaso2() {
-	  try {
-		agencia.registroEmpleado("Lio","2022gloriaeterna" ,"Messi", "Lionel", "+5423234", 36);
-	} catch (NewRegisterException | ImposibleCrearEmpleadoException e) {
+	 EmpleadoPretenso empleado = null; 
+	// int cantidadEmpleados = agencia.getEmpleados().size(); 
+	 try {	
+		empleado = (EmpleadoPretenso) agencia.registroEmpleado("Lio","2022gloriaeterna" ,"Messi", "Lionel", "+5423234", 36);
+		agencia.login("Lio", "2022gloriaeterna");	
+		agencia.crearTicketEmpleado(Constantes.HOME_OFFICE, 1200, Constantes.JORNADA_MEDIA, Constantes.JUNIOR, Constantes.SECUNDARIOS, Constantes.EXP_NADA, empleado);	
+		agencia.cerrarSesion();
+		Assert.assertNotNull("El empleado no deberia ser null", empleado);
+		System.out.println(empleado.getTicket());
+		Assert.assertNotNull("El ticket no deberia ser null, pues el empleado creo uno", empleado.getTicket());
+		Assert.assertEquals("El estado del usuario al cerrar sesion debe ser -1", -1,agencia.getTipoUsuario());
+	 } catch (ImposibleModificarTicketsException |NewRegisterException | ImposibleCrearEmpleadoException | ContraException | NombreUsuarioException e) {
 		e.printStackTrace();
 	}
-	  try {
-		agencia.login("Lio", "2022gloriaeterna");
-	} catch (ContraException | NombreUsuarioException e) {
-		e.printStackTrace();
-	}
-	try {
-		agencia.crearTicketEmpleado(Constantes.HOME_OFFICE, 1200, Constantes.JORNADA_MEDIA, Constantes.JUNIOR, Constantes.SECUNDARIOS, Constantes.EXP_NADA, empleado1);
-	} catch (ImposibleModificarTicketsException e) {
-		e.printStackTrace();
-	}
-	System.out.println(agencia.getTipoUsuario());
-   //hacer los assert
-	
   }
 
+  
   // 3) Un empleador se loguea, el estado de contratacion esta en false, elimina un ticket y cierra sesion
   @Test
   public void testCaso3() {
    
 	agencia.setEstadoContratacion(false);
-		try {
-			Empleador usuario = (Empleador) agencia.login("santi", "456");
-		} catch (ContraException | NombreUsuarioException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			agencia.eliminarTicket();
-		} catch (ImposibleModificarTicketsException e) {
-			e.printStackTrace();
-		}
-		agencia.cerrarSesion();
-		//faltan los asssert
-	
+	Empleador usuario = null;
+	try {
+		usuario = (Empleador) agencia.login("santi", "456");
+	} catch (ContraException | NombreUsuarioException e) {
+		e.printStackTrace();
+	}
+	try {
+		agencia.eliminarTicket();
+	} catch (ImposibleModificarTicketsException e) {
+		e.printStackTrace();
+	}
+	agencia.cerrarSesion();
+
+	Assert.assertNull("El ticket deberia ser null", usuario.getTicket());
+	Assert.assertEquals("El estado del usuario al cerrar sesion debe ser -1", -1,agencia.getTipoUsuario());
 	  
   }
 
@@ -176,28 +131,21 @@ public class TestIntegracion {
   @Test
   public void testCaso4() {
 	  agencia.setEstadoContratacion(true);
-	  try {
-		  	  
+	  try {	  	  
 			  Empleador usuario = (Empleador) agencia.login("santi", "456");
 			  agencia.generaPostulantes();
 			  ArrayList<ClientePuntaje> clientes = usuario.getListaDePostulantes();  
 			  //Se elije el primero...
 			  Cliente candidato = clientes.get(0).getCliente();
-			  usuario.setCandidato(candidato);
+			  usuario.setCandidato(candidato);	
+			  agencia.cerrarSesion();
 			  Assert.assertEquals(candidato, usuario.getCandidato());
-			  agencia.gatillarRonda();
-			  
+			  Assert.assertEquals("El estado del usuario al cerrar sesion debe ser -1", -1,agencia.getTipoUsuario());
 		} catch (ContraException | NombreUsuarioException e) {
 			e.printStackTrace();
-		}
-		
-	
-		//faltan los asssert
-	  
-	
-		  
-		  
+		} 
   }
+  
   
   // 5)El admin se loguea, gatilla ronda y se hacen matchs
   @Test
@@ -214,9 +162,7 @@ public class TestIntegracion {
 	  agencia.gatillarRonda();
 	  agencia.cerrarSesion();
 	
-	  //assert de que el estado de contratacion pasa a false
-	  //assert de que getListaContrataciones.length>0
-	  //assert para cada empleado de que no tienen tickets
-	  //assert tipoUsuario = -1
+	  Assert.assertEquals(agencia.getTipoUsuario(), -1);
+	  Assert.assertEquals("Tiene que haber una contratacion en la agencia", 1, agencia.getContrataciones().size());
   }
 }
